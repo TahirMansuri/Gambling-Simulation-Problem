258a5136d137931e9c850a21e354254b5cd46bcc
TahirMansuri <tprocks786@gmail.com>
2022-01-04 16:19:45 +0530

    ADDED: Gambling Simulation Program

---
+++ src/com/bridgelabz/GamblingSimulationProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class GamblingSimulationProblem {
+    public static void main(String[] args) {
+        //Starting Message for User
+        System.out.println("Welcome to Gambling Simulation Problem developed by Tahir Mansuri.");
+    }
+}
